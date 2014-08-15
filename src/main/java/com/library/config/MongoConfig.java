375289c257a7d14ec8abb52f6ebda82956d16e7f
Dmytro Danilenkov <milgoff@gmail.com>
2014-08-15 12:33:32 +0300

    shift mongo config to separate class

--- src/main/java/com/library/config/MongoConfig.java
+++ src/main/java/com/library/config/MongoConfig.java
@@ -1,28 +1,54 @@
 package com.library.config;
 
-import com.library.repository.RepositoryPackage;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.WriteConcern;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.util.Arrays;
 
-/**
- * Loads properties from classpath
- */
 @Configuration
-//@ComponentScan(basePackages = "com.library")
-//@EnableMongoRepositories(basePackageClasses = RepositoryPackage.class)
 @PropertySource("classpath:mongo.properties")
 public class MongoConfig {
+
+    @Value("${mongo.db}")
+    private String mongoDatabase;
+    @Value("${mongo.host}")
+    private String mongoHostname;
+    @Value("${mongo.port}")
+    private String mongoPort;
+    @Value("${mongo.username}")
+    private String username;
+    @Value("${mongo.password}")
+    private String password;
+
+    public
+    @Bean
+    MongoDbFactory mongoDbFactory() throws Exception {
+        MongoCredential credential = MongoCredential.createMongoCRCredential(username, mongoDatabase, password.toCharArray());
+        MongoClient mongo = new MongoClient(new ServerAddress(mongoHostname, Integer.parseInt(mongoPort)), Arrays.asList(credential));
+        return new SimpleMongoDbFactory(mongo, mongoDatabase);
+
+    }
+
+    public
+    @Bean
+    MongoTemplate mongoTemplate() throws Exception {
+
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+        mongoTemplate.setWriteConcern(WriteConcern.SAFE);
+
+        return mongoTemplate;
+
+    }
 
 
     @Bean
